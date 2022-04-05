9195b5ffe1c81a695ff4003d3c7572c740682514
Manuel Duarte <carlosd99m@gmail.com>
2022-04-05 12:29:51 -0600

    feat: add models

---
+++ lib/api/models/job.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'job.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Job _$JobFromJson(Map<String, dynamic> json) => Job(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      locationNames: json['locationNames'] as String?,
+      isFeatured: json['isFeatured'] as bool? ?? false,
+    );
